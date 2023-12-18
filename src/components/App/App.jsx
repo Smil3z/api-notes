98cc15755121e875819d3b07bcc883f2048e9c30
Stephon Tonge <87331368+Smil3z@users.noreply.github.com>
2023-12-17 19:50:10 -0600

    Initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App () {
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Server Axios Env</h1>
+          <h4>Giphy API</h4>
+        </header>
+        <br/>
+      </div>
+    );
+  
+}
+
+export default App;
